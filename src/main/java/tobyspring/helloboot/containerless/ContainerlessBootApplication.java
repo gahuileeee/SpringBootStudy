33640e0e4bdc422cc62adee33bebc019a9c08955
CT00418 <khlee@nextree.io>
2025-04-19 20:29:29 +0900

    refactor: 독립 실행형 스프링 어플리케이션 - @Configuration 을 이용해 구성정보 전달

--- src/main/java/tobyspring/helloboot/containerless/ContainerlessBootApplication.java
+++ src/main/java/tobyspring/helloboot/containerless/ContainerlessBootApplication.java
@@ -1,36 +1,44 @@
-package tobyspring.helloboot;
+package tobyspring.helloboot.containerless;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
-import tobyspring.helloboot.containerless.HelloController;
-import tobyspring.helloboot.containerless.SimpleHelloService;
 
+@Configuration //구성 정보를 가지고 있는 클래스이다.
 public class ContainerlessBootApplication {
+    @Bean
+    public HelloController helloController (HelloService helloService) {
+        return new HelloController(helloService);
+    }
+
+    @Bean
+    public HelloService helloService () {
+        return new SimpleHelloService();
+    }
 
     public static void main(String[] args) {
-        //Spring Container 를 만들것임
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
-        applicationContext.registerBean(HelloController.class);
-        applicationContext.registerBean(SimpleHelloService.class); //인터페이스가 아닌 정확한 클래스를 빈으로 등록해야 함.
 
-        //스프링 컨테이너가 알아서 빈들을 자기 순서대로 만들어줌
-        //컨트롤러를 만들어야 하는데 파라미터로 헬로 서비스가 필요한 경우 -> 자기의 빈들 다 탐색해서 파라미터로 넘겨주어 빈으로 만듦.
-        //여러개를 빈으로 등록하는경우? -> @Primary 를 통해 기본 빈을 지정하거나 @Qualifier 로 명시적으로 주입할 수도 있음.
-        // 이것을 해주지 않는다면 NoUniqueBeanDefinitionException 에러가 나게됨.
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
+            @Override
+            protected void onRefresh() {
+                //onRefresh 는 ApplicationContext 가 초기화 되는 과정에 호출되는 후처리 훅 메서드.
+                super.onRefresh(); // 생략하면 안 됨. 생략하게 되면 기본적인 Bean 초기화 로직이 날아가게 됨.
 
-        //기존에는 xml 으로 빈을 등록해주고 생성자에다 어떤 빈을 주입할 지도 기술을 했었음
+                // onRefresh 시점 (컨테이너 초기화가 끝난 시점) 에 dispatcherServlet 초기화
+                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                WebServer webServer = serverFactory.getWebServer(servletContext ->
+                        servletContext.addServlet("dispatcherServlet", new DispatcherServlet(this) {
+                        }).addMapping("/*"));
+                webServer.start();
+            }
+        };
 
-        applicationContext.refresh(); //bean object 를 만들어줌 (모든 초기화가 일어남)
+        applicationContext.register(ContainerlessBootApplication.class); //파라미터의 클래스에서 @Bean 메소드들을 읽고 빈 정의로 등록하라
 
-        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-
-        WebServer webServer = serverFactory.getWebServer(servletContext ->
-                servletContext.addServlet("dispatcherServlet", new DispatcherServlet(applicationContext) {
-                }).addMapping("/*"));
-
-        webServer.start(); //Tomcat Servlet Container 가 동작하게 됨.
+        applicationContext.refresh();
     }
 }
