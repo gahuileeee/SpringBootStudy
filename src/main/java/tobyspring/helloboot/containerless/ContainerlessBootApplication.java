ef784e34982c20b36e6ba349bd796629a04d068b
이가희 <gahui446@gmail.com>
2025-04-20 18:00:05 +0900

    refactor : 독립 실행형 스프링 어플래케이션 - 부트 시작을 메소드화 처리

--- src/main/java/tobyspring/helloboot/containerless/ContainerlessBootApplication.java
+++ src/main/java/tobyspring/helloboot/containerless/ContainerlessBootApplication.java
@@ -1,36 +1,26 @@
 package tobyspring.helloboot.containerless;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
 @ComponentScan
-//@Component 를 인식해서 빈으로 등록함 (메타 어노테이션도 포함)
 public class ContainerlessBootApplication {
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory(){
+        return new TomcatServletWebServerFactory();
+    }
+
+    @Bean
+    public DispatcherServlet dispatcherServlet(){
+        return new DispatcherServlet();
+    }
 
     public static void main(String[] args) {
-
-        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-            @Override
-            protected void onRefresh() {
-                //onRefresh 는 ApplicationContext 가 초기화 되는 과정에 호출되는 후처리 훅 메서드.
-                super.onRefresh(); // 생략하면 안 됨. 생략하게 되면 기본적인 Bean 초기화 로직이 날아가게 됨.
-
-                // onRefresh 시점 (컨테이너 초기화가 끝난 시점) 에 dispatcherServlet 초기화
-                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-                WebServer webServer = serverFactory.getWebServer(servletContext ->
-                        servletContext.addServlet("dispatcherServlet", new DispatcherServlet(this) {
-                        }).addMapping("/*"));
-                webServer.start();
-            }
-        };
-        applicationContext.register(ContainerlessBootApplication.class); //파라미터의 클래스에서 @Bean 메소드들을 읽고 빈 정의로 등록하라
-
-        applicationContext.refresh();
+        MySpringApplication.run(ContainerlessBootApplication.class, args); //SpringBootApplication.run 과 유사하게 동작
     }
 }
