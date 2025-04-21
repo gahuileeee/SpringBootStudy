48fcbd7f79a3e7384c02ffdffa23173beca035e4
CT00418 <khlee@nextree.io>
2025-04-22 08:38:06 +0900

    refactor: 독립 실행형 스프링 어플리케이션 - Config 클래스 어노테이션 변경 (Configuration -> MyAutoConfiguration)

--- src/main/java/tobyspring/helloboot/config/autoconfig/DispatcherServletConfig.java
+++ src/main/java/tobyspring/helloboot/config/autoconfig/DispatcherServletConfig.java
@@ -1,10 +1,10 @@
 package tobyspring.helloboot.config.autoconfig;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
+import tobyspring.helloboot.config.MyAutoConfiguration;
 
-@Configuration
+@MyAutoConfiguration
 public class DispatcherServletConfig {
     @Bean
     public DispatcherServlet dispatcherServlet(){
--- src/main/java/tobyspring/helloboot/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/tobyspring/helloboot/config/autoconfig/TomcatWebServerConfig.java
@@ -3,9 +3,9 @@ package tobyspring.helloboot.config.autoconfig;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import tobyspring.helloboot.config.MyAutoConfiguration;
 
-@Configuration
+@MyAutoConfiguration
 public class TomcatWebServerConfig {
     @Bean
     public ServletWebServerFactory servletWebServerFactory(){
