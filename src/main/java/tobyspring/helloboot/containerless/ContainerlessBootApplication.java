07d424d3cd3f22e83d7f5f73f3c9c6e29043a843
CT00418 <khlee@nextree.io>
2025-04-21 10:49:59 +0900

    refactor: 독립 실행형 스프링 어플리케이션 - 자동 구성 정보 패키지 분리, @Import 하드 코딩으로 Config.class 직접 등록

--- src/main/java/tobyspring/helloboot/containerless/ContainerlessBootApplication.java
+++ src/main/java/tobyspring/helloboot/containerless/ContainerlessBootApplication.java
@@ -1,11 +1,8 @@
 package tobyspring.helloboot.containerless;
 
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.DispatcherServlet;
+import tobyspring.helloboot.config.MySpringBootApplication;
 
-@MySpringBootAnnotation
+@MySpringBootApplication
 public class ContainerlessBootApplication {
     public static void main(String[] args) {
         MySpringApplication.run(ContainerlessBootApplication.class, args); //SpringApplication.run 과 유사하게 동작
