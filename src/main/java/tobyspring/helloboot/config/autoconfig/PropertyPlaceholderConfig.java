b1320134bd89f39e012f961282f183b369bd3d6b
이가희 <gahui446@gmail.com>
2025-04-27 21:51:56 +0900

    refactor : @Value 를 통해 contextPath 받기

---
+++ src/main/java/tobyspring/helloboot/config/autoconfig/PropertyPlaceholderConfig.java
@@ -0,0 +1,14 @@
+package tobyspring.helloboot.config.autoconfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import tobyspring.helloboot.config.MyAutoConfiguration;
+
+@MyAutoConfiguration
+public class PropertyPlaceholderConfig {
+    @Bean
+    PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer(){
+        System.out.println("??");
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+}
