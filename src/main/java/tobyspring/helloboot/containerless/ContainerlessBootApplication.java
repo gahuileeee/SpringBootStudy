019fae34fd2f60a6ec6e27e36468314cd968912e
CT00418 <khlee@nextree.io>
2025-04-30 10:47:59 +0900

    feat: 매칭되는 자동 구성 빈들 확인하는 메소드 생성

--- src/main/java/tobyspring/helloboot/containerless/ContainerlessBootApplication.java
+++ src/main/java/tobyspring/helloboot/containerless/ContainerlessBootApplication.java
@@ -1,8 +1,11 @@
 package tobyspring.helloboot.containerless;
 
 import jakarta.annotation.PostConstruct;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionEvaluationReport;
+import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 //@MySpringBootApplication
@@ -12,6 +15,24 @@ public class ContainerlessBootApplication {
 
     public ContainerlessBootApplication(JdbcTemplate jdbcTemplate){
         this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Bean
+    ApplicationRunner run(ConditionEvaluationReport report){
+        return arg -> {
+            report.getConditionAndOutcomesBySource().entrySet().stream()
+                    .filter(co -> co.getValue().isFullMatch())
+                    .filter(co -> !co.getKey().contains("Jmx"))
+                    .forEach(co->{
+                        System.out.println(co.getKey());
+                        co.getValue().forEach(c->{
+                            System.out.println(c.getOutcome());
+                            //어떤 조건을 통과했는지도 볼 수 있음.
+                        });
+                        System.out.println();
+                    });
+        };
+        //매칭된 것만 봄
     }
 
     @PostConstruct
